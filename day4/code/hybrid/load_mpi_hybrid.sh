2f0211eec78dbdd6614d08698416f6c5c1b1fe11
Simon Scheidegger of group tsmith <ss3835@grace2.grace.hpc.yale.internal>
2018-09-04 11:39:56 -0400

    updated hybrid code

---
+++ day4/code/hybrid/load_mpi_hybrid.sh
@@ -0,0 +1,1 @@
+ module load MPI/OpenMPI/1.8.1-intel14
--- day4/code/hybrid/submit_hybrid_yale.sh
+++ day4/code/hybrid/submit_hybrid_yale.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=8
 #SBATCH --cpus-per-task=5
 #SBATCH --nodes=2
-#SBATCH --time=10:00
+#SBATCH --time=00:10:00
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-mpirun hello_hybrid.mpi
+mpirun 1a.hello_world_hybrid.exec
