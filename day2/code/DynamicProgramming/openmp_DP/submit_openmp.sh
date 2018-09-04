5f75418c36098aaebfd59b38052673eaf722def5
Simon Scheidegger of group tsmith <ss3835@grace1.grace.hpc.yale.internal>
2018-09-04 11:21:30 -0400

    slurm script

--- day2/code/DynamicProgramming/openmp_DP/submit_openmp.sh
+++ day2/code/DynamicProgramming/openmp_DP/submit_openmp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=omp_job
-#SBATCH --output=DP.out
+#SBATCH --output=DP_job.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --time=10:00
+#SBATCH --cpus-per-task=12
+#SBATCH --time=00:00:10
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
