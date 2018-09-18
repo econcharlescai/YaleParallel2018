c43711fea38c7d2dfc7c4d8ed56b1ec692de6787
simon <simon.scheidegger@gmail.com>
2018-09-18 09:14:34 -0400

    added mpi examples

--- day3/code/MPI/submit_MPI_yale.sh
+++ day3/code/MPI/submit_MPI_yale.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=mpi
 #SBATCH --output=mpi_job.txt
-#SBATCH --ntasks=120
-#SBATCH --time=10:00
+#SBATCH --ntasks=20
+#SBATCH --time=00:01:00
 
 
 mpirun 1a.hello_world_mpi_cpp.exec
